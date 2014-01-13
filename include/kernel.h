5085400db8412c40b0da28dbba423a036d724020
Ilya Staheev <istaheev@gmail.com>
2014-01-13 02:06:31 +0100

    gdt_64 moved to higher half

---
+++ include/kernel.h
@@ -0,0 +1,11 @@
+#ifndef __KERNEL_H__
+#define __KERNEL_H__ 1
+
+// The kernel is linked to this virtual address.
+// linker.ld should be updated accordingly if changed.
+#define KERNEL_VMA 0xFFFFFFFF80000000
+
+// Size of the kernel stack
+#define KERNEL_STACK_SIZE 0x2000
+
+#endif
